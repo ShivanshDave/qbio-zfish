cb4941d2ddeea047dbc5fb53b5a32de5e238a3f6
Shivansh <dave.shivansh@gmail.com>
2019-01-20 00:04:13 +0530

    DAT format small change

--- plot-code/readh5.py
+++ plot-code/readh5.py
@@ -6,13 +6,14 @@ f = h5py.File(h5file, 'r')
 
 dataset = f['df_with_missing/table']
 
-outputfile = h5file.split("DeepCut")[0] + ".dat"
+outputfile = h5file.split("DeepCut")[0] + ".mp4.dat"
 
-with open(outputfile, 'w') as outf:
+with open(outputfile, 'a') as outf:
 
-    outf.write("{0} {1} {2} {3} {4}\n".format("1:frame","2:xhead","3:yhead","4:xtail","5:ytail" ))
+    outf.write("# DeepLabCut data points \n# 1:frame 2:xhead 3:yhead 4:xtail 5:ytail\n")
     
     for d in dataset:
         
         if d[0]>5: outf.write("{0} {1} {2} {3} {4}\n".format(d[0],d[1][0],d[1][1],d[1][3],d[1][4]))
         
+
