7a43d8e2bfb3a3aea3e8843ef6d76df80812e57f
Shivansh <dave.shivansh@gmail.com>
2019-05-13 22:13:07 +0530

    init-analyisis-read-files

---
+++ analysis-matlab/trigger_fish_plot.m
@@ -0,0 +1,5 @@
+% Read all videos in an experimen
+
+% Read timestamp
+ts = readtimestamp('C:\Users\sdd50\work\zfish\digitized_data\stimulus-timestamp-dictionary.csv');
+find(ts.VIDEONAME=='f1-static-ds-vid_2018-08-15_17-33-07.mp4')
