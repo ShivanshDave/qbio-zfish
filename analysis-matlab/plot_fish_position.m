88bec24e7a7f831a4482337ef88d6a1f9f6a26b6
Shivansh <dave.shivansh@gmail.com>
2019-06-10 10:03:09 +0530

    position-plot-restructure

--- analysis-matlab/plot_fish_position.m
+++ analysis-matlab/plot_fish_position.m
@@ -15,41 +15,46 @@ exp_name = {
 
 
 % Plot params
-nCol = 2; nRow = 2;
-up_T1 = 1; up_T2 = 2;
-down_T1 = 3; down_T2 = 4;
+plt.nCol = 2; plt.nRow = 2;
+plt.T1.X = 1; plt.T2.X = 2;
+plt.T1.Y = 3; plt.T2.Y = 4;
 
-fish_color = [ [0, 0.4470, 0.7410];[0.8500, 0.3250, 0.0980];...
-               [0.9290, 0.6940, 0.1250];[0.4940, 0.1840, 0.5560] ];
-           
 % axes for - cropped or full
-if cropped_plot; ax = [-500 1500 nan nan]; else ax = [-3e3 5e3  nan nan]; end
+if cropped_plot
+    plt.ax = [-500 1500 nan nan]; 
+else
+    plt.ax = [-3e3 5e3  nan nan]; 
+end
 % axes for - zeroed or absolute position
-if zeroed_plot; ax(3:4) = [-150 150]; else ax(3:4) = [0 300]; end
+if zeroed_plot
+    plt.ax(3:4) = [-150 150]; 
+else
+    plt.ax(3:4) = [0 300]; 
+end
 
-% LPF  
-cutoff = 150; % Hz
-sr = 1e3; % FPS            
-lowpass = designfilt('lowpassiir','FilterOrder',4, ...
-          'PassbandFrequency',cutoff,'PassbandRipple',0.01,'SampleRate',sr);
+plt.ytickgapX = 50; % mm
+plt.ytickgapY = 50; % mm
       
 for i=1:5
-    h1 = figure('Name','FishTrajectories','Units','centimeters','Position', [-2, -2, 29.7, 21]);
+    plt.h1 = figure('Name','FishTrajectories','Units','centimeters','Position', [-2, -2, 29.7, 21]);
     
     T1 = exp_name{i,1}; % Stim Type 1
     T2 = exp_name{i,2}; % Stim Type 2
 
-    title=['', strrep(T1,'_','-'),' vs ',strrep(T2,'_','-')];
+    title=['Raw data plots : ', strrep(T1,'_','-'),' vs ',strrep(T2,'_','-')];
     suptitle(title);
 
     data = struct;
-    data = get_data_single_type(data, exp, T1, lowpass, zeroed_plot);
-    data = get_data_single_type(data, exp, T2, lowpass, zeroed_plot);
+    data = get_data_single_type(data, exp, T1, zeroed_plot);
+    data = get_data_single_type(data, exp, T2, zeroed_plot);
+    
+    plot_single_type(plt, data, T1, plt.T1);
+    plot_single_type(plt, data, T2, plt.T2);
     
     
-    fname = sprintf('plot-%s--%s',expT1_name,expT2_name);
+    fname = sprintf('plot-%s--%s',T1,T2);
     fpath = fullfile(plot_dir,plot_type,fname);
-    savefig(h1,[fpath,'.fig']);
+    savefig(plt.h1,[fpath,'.fig']);
     TufteStyleC;
     orient(h1,'landscape');
     set(gcf,'PaperPositionMode','auto');
@@ -59,9 +64,14 @@ end
 
 end
 
-function data = get_data_single_type(data, exp, T, lowpass, zeroed_plot)
+function data = get_data_single_type(data, exp, T, zeroed_plot)
 
     px2mm = 1/4.725;
+    cutoff = 150; % Hz
+    sr = 1e3; % FPS            
+    lowpass = designfilt('lowpassiir','FilterOrder',4, ...
+              'PassbandFrequency',cutoff,'PassbandRipple',0.01,'SampleRate',sr);
+
     for f=1:4
         fishID = ['f',num2str(f)];    
         
@@ -86,10 +96,9 @@ function data = get_data_single_type(data, exp, T, lowpass, zeroed_plot)
             end                        
             
             [data.(T).(fish).Xrho, data.(T).(fish).Xpval] = ...   
-                corr(data.(T).(fish).t',data.(T).(fish).X,'Type','Spearman');
+                corr(data.(T).(fish).t,data.(T).(fish).X,'Type','Spearman');
             [data.(T).(fish).Yrho, data.(T).(fish).Ypval] = ...   
-                corr(data.(T).(fish).t',data.(T).(fish).Y,'Type','Spearman');
-                         
+                corr(data.(T).(fish).t,data.(T).(fish).Y,'Type','Spearman');                        
         end       
     end
 end
@@ -122,46 +131,38 @@ function data = remove_artifacts(data, lowpass)
 end
 
 
-function plot_single_type()
-
-           hUpT1 = subplot(nRow,nCol,up_T1); hold on;
-            plot(data.t,data.X, 'Color', fish_color(f,:));
-            hDownT1 = subplot(nRow,nCol,down_T1); hold on;
-            plot(data.t,data.Y, 'Color', fish_color(f,:));
-            
-            
-            hUpT2 = subplot(nRow,nCol,up_T2); hold on;
-            plot(data.t,data.X, 'Color', fish_color(f,:));            
-            hDownT2 = subplot(nRow,nCol,down_T2); hold on;
-            plot(data.t,data.Y, 'Color', fish_color(f,:));
-          
-            
- 
+function plot_single_type(plt, data, T, pax)
     
-    hUpT1 = subplot(nRow,nCol,up_T1); hold on;
-    axis(ax);
-    yticks(ax(3):50:ax(4));
-%     yticklabels({'Front', 'center', 'Back'});
-    ylabel({'X--dir position ( mm )', '( Neg.: Up-stream, Pos.: Down-stream )'});
+    fish_color = [ [0, 0.4470, 0.7410];[0.8500, 0.3250, 0.0980];...
+               [0.9290, 0.6940, 0.1250];[0.4940, 0.1840, 0.5560] ];
+    for f=1:4
+        for t=1:4
+            fish = ['f',num2str(f),'t',num2str(t)];
 
-    hUpT2 = subplot(nRow,nCol,up_T2); hold on;
-    axis(ax);
-    yticks(ax(3):50:ax(4));
-%     yticklabels({'Front', 'center', 'Back'});
-
-    hDownT1 = subplot(nRow,nCol,down_T1); hold on;
-    axis(ax);
-    yticks(ax(3):50:ax(4));
-%     yticklabels({'Righ-wall', 'middle', 'Left-wall'});
-    xlabel({'Time (milliseconds)',expT1_name});
-    ylabel({'Y-dir position ( mm )', '( Neg.: Righ-wall, Pos.: Left-wall )'});
-
-    hDownT2 = subplot(nRow,nCol,down_T2); hold on;
-    axis(ax);
-    yticks(ax(3):50:ax(4));
-%     yticklabels({'Righ-wall', 'middle', 'Left-wall'});
-    xlabel({'Time (milliseconds)',expT2_name});   
+            hUpT = subplot(plt.nRow,plt.nCol,pax.X); hold on;
+            plot(data.(T).(fish).t,data.(T).(fish).X, 'Color', fish_color(f,:));
+            
+            hDownT = subplot(plt.nRow,plt.nCol,pax.Y); hold on;
+            plot(data.(T).(fish).t,data.(T).(fish).Y, 'Color', fish_color(f,:));
+        end
+    end
     
-    linkaxes([hUpT1,hUpT2,hDownT1,hDownT2],'x');
+    hUpT = subplot(plt.nRow,plt.nCol,pax.X); hold on;
+    axis(plt.ax);
+    yticks(plt.ax(3):plt.ytickgapX:plt.ax(4));
+%     yticklabels({'Front', 'center', 'Back'});
+%     ylabel({'X--dir position ( mm )', '( Neg.: Up-stream, Pos.: Down-stream )'});
+    ylabel({'X--dir position ( mm )', '<--- Up-stream  -Middle-  Down-stream --->'});
+
+
+    hDownT = subplot(plt.nRow,plt.nCol,pax.Y); hold on;
+    axis(plt.ax);
+    yticks(plt.ax(3):plt.ytickgapY:plt.ax(4));
+%     yticklabels({'Righ-wall', 'middle', 'Left-wall'});
+%     ylabel({'Y-dir position ( mm )', '( Neg.: Righ-wall, Pos.: Left-wall )'});
+    ylabel({'Y-dir position ( mm )', '<--- Righ-wall -Middle- Left-wall --->'});
+    xlabel({'Time (milliseconds)',strrep(T,'_','-')});
+    
+    linkaxes([hUpT,hDownT,],'x');
 
 end
